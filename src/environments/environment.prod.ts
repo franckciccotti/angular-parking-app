704f32d943d583b29b1a7263773241fdecf650d5
Franck CICCOTTI <franckciccotti@me.com>
2024-01-04 22:54:24 +0100

    modif

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'http://park-nantes-app-fc-8053446c6978.herokuapp.com/api'
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8080/api'
+    production: true,
+    apiUrl: 'http://park-nantes-app-fc-8053446c6978.herokuapp.com/api'
 };
