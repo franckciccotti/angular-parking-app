b364dce4ca7e15c23e1fc199b63527339a3c7335
Franck CICCOTTI <franckciccotti@me.com>
2024-01-04 22:22:30 +0100

    fix bug adress heroku

--- src/app/parking.service.ts
+++ src/app/parking.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ParkingInfo } from './parkingInfo';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class ParkingService {
   constructor(private httpClient: HttpClient) { }
 
   getParkings(): Observable<ParkingInfo[]> {
-    return this.httpClient.get<ParkingInfo[]>('http://localhost:8080/api/parkings');
+    // return this.httpClient.get<ParkingInfo[]>('http://localhost:8080/api/parkings');
+    return this.httpClient.get<ParkingInfo[]>(environment.apiUrl + '/parkings');
   }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://park-nantes-app-fc-8053446c6978.herokuapp.com/api'
+};
